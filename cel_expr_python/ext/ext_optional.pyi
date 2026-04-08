02cc0742c6a8f5a33145254c7f53078ba634133d
CEL Dev Team <cel-dev@google.com>
2026-04-08 09:17:59 -0700

    Add type stubs
    PiperOrigin-RevId: 896539483

---
+++ cel_expr_python/ext/ext_optional.pyi
@@ -0,0 +1,4 @@
+from cel_expr_python import cel
+
+class ExtOptional(cel.CelExtensionBase):
+  def __init__(self) -> None: ...
---
+++ cel_expr_python/ext/ext_proto.pyi
@@ -0,0 +1,4 @@
+from cel_expr_python import cel
+
+class ExtProto(cel.CelExtensionBase):
+  def __init__(self) -> None: ...
