13ff88d9deaff848302ea299d46f3c61ff13681d
Dmitri Plotnikov <dplotnikov@google.com>
2026-05-26 14:36:26 -0700

    MacOS presubmit and release job stubs
    PiperOrigin-RevId: 921689035

---
+++ release/kokoro/presubmit_macos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Cel-expr-python build and test on MacOS"
+echo "TODO(b/507567432): implement presubmit build for MacOS."
+
+exit 0
---
+++ release/kokoro/release_macos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Cel-expr-python release build on MacOS"
+echo "TODO(b/507567432): implement release build for MacOS."
+
+exit 0
